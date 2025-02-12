17d0e25ab36198daa57438ab7fe9acdf33776c3b
Ramatoulaye9 <ramatoulayecam9@gmail.com>
2025-02-12 14:15:23 +0000

    Cors géré

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv'); // Pour charger les variables d'environnement
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv"); // Pour charger les variables d'environnement
 const app = express();
-const userRoutes = require('./routes/userRoute')
-const houseRoutes = require('./routes/houseRoute')
-const reservationRoutes = require('./routes/reservationRoute')
-const path = require('path'); // Importation de path
-const cors = require('cors');
+const userRoutes = require("./routes/userRoute");
+const houseRoutes = require("./routes/houseRoute");
+const reservationRoutes = require("./routes/reservationRoute");
+const path = require("path"); // Importation de path
+const cors = require("cors");
 
 dotenv.config(); // Charger les variables d'environnement depuis un fichier .env
 
 app.use(express.json()); // Middleware pour traiter les requêtes JSON
 
 // app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: "http://localhost:4200", // ou utilisez '*' pour autoriser toutes les origines (attention en prod)
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(cors());    //Enable CORS
-app.get('/', (req, res) => {
-    res.send("Salut, API");
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Salut, API");
 });
 
 // Serve the 'uploads' folder as static files
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/houses', houseRoutes)
-app.use('/users', userRoutes)
-app.use('/reservations', reservationRoutes)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/houses", houseRoutes);
+app.use("/users", userRoutes);
+app.use("/reservations", reservationRoutes);
 
 // Vérifiez si la variable d'environnement pour MongoDB est définie
 if (!process.env.MONGO_ENV) {
-    console.error("La variable d'environnement MONGO_ENV n'est pas définie.");
-    process.exit(1);
+  console.error("La variable d'environnement MONGO_ENV n'est pas définie.");
+  process.exit(1);
 }
 
-mongoose.connect(process.env.MONGO_ENV)
-    .then(() => {
-        console.log("Connexion à MongoDB réussie!");
-        const port = process.env.PORT || 3000;
-        app.listen(port, () => {
-            console.log('Le serveur démarre au port: 3000');
-        });
-    })
-    .catch((error) => {
-        console.error("Connexion échouée :", error.message);
+mongoose
+  .connect(process.env.MONGO_ENV)
+  .then(() => {
+    console.log("Connexion à MongoDB réussie!");
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log("Le serveur démarre au port: 3000");
     });
+  })
+  .catch((error) => {
+    console.error("Connexion échouée :", error.message);
+  });
